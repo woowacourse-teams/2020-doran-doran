e3eca3ca40d1fdb0fdefb521080c8a9af9643715
yebink <yebin825@gmail.com>
2020-07-08 15:31:16 +0900

    project init

---
+++ src/main/java/com/grasshouse/dorandoran/DoranDoranApplication.java
@@ -0,0 +1,13 @@
+package com.grasshouse.dorandoran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoranDoranApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoranDoranApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grasshouse/dorandoran/DoranDoranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grasshouse.dorandoran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoranDoranApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
