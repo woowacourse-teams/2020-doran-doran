054dfc55742bdbae2ab7e2a515ee42d3f9daf724
yejiahn <yeppilisa@naver.com>
2020-07-09 15:16:59 +0900

    feature/2 feat: 지도화면 띄우는 기능 구현
     - Map 컴포넌트 구현
     - 기본 생성된 불필요 파일 삭제

--- front/.prettierrc.js
+++ front/.prettierrc.js
@@ -9,4 +9,5 @@ module.exports = {
   bracketSpacing: true, // 객체리터럴에서 { } 사이에 공백을 넣을 것인지
   jsxBracketSameLine: false, // 여러줄의 JSX 요소가 있을때, > 를 마지막 줄의 끝부분에서 닫을 것인지
   arrowParens: "always", // (x) => x : always | x => x : avoid
+  printWidth: 80,
 };
