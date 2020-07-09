9994c17dcab96be6d1ae845f9a653f4a716d948d
juyeongjung <juyeong.dev@gmail.com>
2020-07-09 17:01:17 +0900

    feature/7 feat: 글 조회 ServiceTest

--- src/test/java/com/grasshouse/dorandoran/post/service/PostServiceTest.java
+++ src/test/java/com/grasshouse/dorandoran/post/service/PostServiceTest.java
@@ -2,12 +2,16 @@ package com.grasshouse.dorandoran.post.service;
 
 import static com.grasshouse.dorandoran.fixture.LocationFixture.LOCATION;
 import static com.grasshouse.dorandoran.fixture.MemberFixture.MEMBER;
+import static com.grasshouse.dorandoran.fixture.PostFixture.POST;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.grasshouse.dorandoran.member.domain.Member;
 import com.grasshouse.dorandoran.member.repository.MemberRepository;
+import com.grasshouse.dorandoran.post.repository.PostRepository;
 import com.grasshouse.dorandoran.post.service.dto.PostCreateRequest;
 import com.grasshouse.dorandoran.post.service.dto.PostCreateResponse;
+import com.grasshouse.dorandoran.post.service.dto.PostResponse;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +25,9 @@ class PostServiceTest {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private PostRepository postRepository;
 
     @DisplayName("글이 작성된다.")
     @Test
@@ -36,5 +43,15 @@ class PostServiceTest {
         assertThat(createResponse.getId()).isNotNull();
     }
 
+    @DisplayName("글을 조회한다.")
+    @Test
+    void showPostsTest() {
+        memberRepository.save(MEMBER);
+        postRepository.save(POST);
 
+        List<PostResponse> postResponses = postService.showPosts();
+
+        assertThat(postResponses).hasSize(1);
+        assertThat(postResponses.get(0).getContent()).isEqualTo(POST.getContent());
+    }
 }
