1f4dabff0f915f79107cdbf39781547405765315
juyeongjung <juyeong.dev@gmail.com>
2020-07-13 18:04:37 +0900

    feature/6 feat: Post 더미 데이터 생성

---
+++ front/src/utils/mock-data.js
@@ -0,0 +1,34 @@
+const MOCK_POSTS = [
+  {
+    id: 1,
+    content: "내용1",
+    location: {
+      longitude: 127.105392,
+      latitude: 37.515749,
+    },
+  },
+  {
+    id: 2,
+    content: "내용2내용2내용2",
+    location: {
+      longitude: 127.103472,
+      latitude: 37.514906,
+    },
+  },
+  {
+    id: 3,
+    content: "내용3내용3내용3내용3내용3",
+    location: {
+      longitude: 127.103129,
+      latitude: 37.516634,
+    },
+  },
+  {
+    id: 4,
+    content: "내용4내용4내용4내용4내용4내용4내용4내용4내용4내용4",
+    location: {
+      longitude: 127.100629,
+      latitude: 37.515911,
+    },
+  },
+];
