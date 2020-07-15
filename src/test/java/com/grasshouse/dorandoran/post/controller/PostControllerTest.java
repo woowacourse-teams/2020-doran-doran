76bc4aca7cc4fa33aec794b2a1e3fa1b8832ff5e
yebink <yebin825@gmail.com>
2020-07-15 10:46:03 +0900

    feature/19 test: ControllerTest 공통 기능 분리
     - CommonControllerTest를 상속받는 구조로 변경

--- src/test/java/com/grasshouse/dorandoran/post/controller/PostControllerTest.java
+++ src/test/java/com/grasshouse/dorandoran/post/controller/PostControllerTest.java
@@ -15,45 +15,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.grasshouse.dorandoran.common.CommonControllerTest;
 import com.grasshouse.dorandoran.post.service.PostService;
 import com.grasshouse.dorandoran.post.service.dto.PostCreateRequest;
 import com.grasshouse.dorandoran.post.service.dto.PostCreateResponse;
 import com.grasshouse.dorandoran.post.service.dto.PostResponse;
 import java.util.Arrays;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-class PostControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
+class PostControllerTest extends CommonControllerTest {
 
     @MockBean
     private PostService postService;
-
-    @BeforeEach
-    void setUp(WebApplicationContext webApplicationContext) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-            .addFilter(new CharacterEncodingFilter("UTF-8", true))
-            .build();
-    }
 
     @DisplayName("글을 작성한다.")
     @Test
