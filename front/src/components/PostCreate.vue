52bd2f19f5ad7b38c26657287601052b060157ba
chws <rebeccachoosir@gmail.com>
2020-07-20 10:28:16 +0900

    feature/4 refactor: PostCreate.vue의 indent변경

--- front/src/components/PostCreate.vue
+++ front/src/components/PostCreate.vue
@@ -1,40 +1,42 @@
 <template>
-    <v-form class="pa-7">
-      <PostCreateMap />
-      <div class="content">
-        <v-textarea
-          v-model="content"
-          type="text"
-          name="content"
-          placeholder="글 내용을 입력해주세요."
-          maxlength="200"
-          rows="10"
-          :counter="200"
-          required
-          solo
-          autofocus
-          no-resize
-        />
-      </div>
-      <v-btn
-        @click="createPost"
-        class="float-right"
-        color="amber accent-4"
-        type="submit"
-        >등록</v-btn
-      >
-    </v-form>
+  <v-form class="pa-7">
+    <PostCreateMap />
+    <div class="content">
+      <v-textarea
+        v-model="content"
+        type="text"
+        name="content"
+        placeholder="글 내용을 입력해주세요."
+        maxlength="200"
+        rows="10"
+        :counter="200"
+        required
+        solo
+        autofocus
+        no-resize
+      />
+    </div>
+    <v-btn
+      @click="createPost"
+      class="float-right"
+      color="amber accent-4"
+      type="submit"
+      >등록</v-btn
+    >
+  </v-form>
 </template>
 
 <script>
 import PostCreateMap from "./PostCreateMap";
 export default {
+  components: {
+    PostCreateMap,
+  },
   data() {
     return {
       content: "",
     };
   },
-  components: { PostCreateMap },
   methods: {
     createPost() {
       this.$store.dispatch("post/createPost", this.content);
