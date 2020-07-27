8f2b4dc1dff7f8eaef83ec8d14253637bcc58cae
yebink <yebin825@gmail.com>
2020-07-27 20:27:58 +0900

    feature/32 feat: 글 작성 플로팅 버튼 컴포넌트 추가

---
+++ front/src/components/PostCreateButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-btn class="mb-12" fab dark large absolute right bottom color="amber accent-4">
+    <v-icon dark>mdi-pencil</v-icon>
+  </v-btn>
+</template>
+
+<script>
+  export default {
+    name: "PostCreateButton"
+  }
+</script>
+
+<style scoped>
+
+</style>
