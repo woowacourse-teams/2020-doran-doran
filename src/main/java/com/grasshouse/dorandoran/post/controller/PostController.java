7fee4dbd718c20469e9c1fd33e2ab5946a42f973
yejiahn <yeppilisa@naver.com>
2020-07-28 17:27:22 +0900

    feature/39 feat: PostController에 크로스오리진 적용

--- src/main/java/com/grasshouse/dorandoran/post/controller/PostController.java
+++ src/main/java/com/grasshouse/dorandoran/post/controller/PostController.java
@@ -7,6 +7,7 @@ import com.grasshouse.dorandoran.post.service.dto.PostResponse;
 import java.net.URI;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/posts")
 public class PostController {
