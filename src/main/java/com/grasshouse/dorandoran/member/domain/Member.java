c6b5aac59de30a767a6a611d6071c07eace1b8e5
yejiahn <yeppilisa@naver.com>
2020-08-03 14:32:59 +0900

    feature/22 feat: Member 클래스에 createdAt, updatedAt 필드 추가

--- src/main/java/com/grasshouse/dorandoran/member/domain/Member.java
+++ src/main/java/com/grasshouse/dorandoran/member/domain/Member.java
@@ -2,10 +2,12 @@ package com.grasshouse.dorandoran.member.domain;
 
 import com.grasshouse.dorandoran.comment.domain.Comment;
 import com.grasshouse.dorandoran.post.domain.Post;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -14,12 +16,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Member {
 
     @Id
@@ -28,6 +34,12 @@ public class Member {
 
     @Column(unique = true, nullable = false)
     private String nickname;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
     @Builder.Default
     @OneToMany(mappedBy = "author")
