2288d48b629a064637a6dff23a2f2b1577bcad5a
chws <rebeccachoosir@gmail.com>
2020-08-03 14:18:03 +0900

    feature/22 feat: Post, Comment 클래스에 createdAt 필드 추가

--- src/main/java/com/grasshouse/dorandoran/DoranDoranApplication.java
+++ src/main/java/com/grasshouse/dorandoran/DoranDoranApplication.java
@@ -2,8 +2,10 @@ package com.grasshouse.dorandoran;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DoranDoranApplication {
 
     public static void main(String[] args) {
