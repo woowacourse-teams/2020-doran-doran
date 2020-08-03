b5fa9ffff32a5fe1beda9f0a0b4d2a4df34ba990
yebink <yebin825@gmail.com>
2020-08-03 14:55:56 +0900

    feature/54 feat: Modal 컴포넌트 생성

---
+++ front/src/components/Modal.vue
@@ -0,0 +1,93 @@
+<template>
+  <transition name="modal" appear>
+    <div class="modal-mask" @click.self="$emit('close')">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-content">
+            <slot />
+          </div>
+
+          <footer class="modal-footer">
+            <slot name="footer">
+              <button
+                class="modal-default-button float-right"
+                color="amber accent-4"
+                @click="$emit('close')"
+              >
+                디폴트: 등록
+              </button>
+            </slot>
+          </footer>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+};
+</script>
+
+<style scoped>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 80%;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-content {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  float: right;
+}
+
+/*
+   * The following styles are auto-applied to elements with
+   * transition="modal" when their visibility is toggled
+   * by Vue.js.
+   *
+   * You can easily play with the modal transition by editing
+   * these styles.
+   */
+
+.modal-enter .modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container .modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
