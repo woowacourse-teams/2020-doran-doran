4940d94415dcf208d00a763712250dba1a25f51d
chws <rebeccachoosir@gmail.com>
2020-08-05 20:13:43 +0900

    pr revert practice

--- src/main/java/com/grasshouse/dorandoran/DoranDoranApplication.java
+++ src/main/java/com/grasshouse/dorandoran/DoranDoranApplication.java
@@ -8,6 +8,7 @@ public class DoranDoranApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DoranDoranApplication.class, args);
+        System.out.println("pr revert practice");
     }
 
 }
