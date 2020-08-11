f26b836bcd76fbf0458d2e9511727738d7f195b0
juyeongjung <juyeong.dev@gmail.com>
2020-08-11 21:07:05 +0900

    feature/82 feature: 미 인증된 사용자 Exception 관리

--- src/main/java/com/grasshouse/dorandoran/common/CommonAdvice.java
+++ src/main/java/com/grasshouse/dorandoran/common/CommonAdvice.java
@@ -1,6 +1,7 @@
 package com.grasshouse.dorandoran.common;
 
 import com.grasshouse.dorandoran.common.exception.ExpectedException;
+import com.grasshouse.dorandoran.common.exception.InvalidAuthenticationException;
 import com.grasshouse.dorandoran.common.exception.dto.ErrorResponse;
 import java.util.stream.Collectors;
 import javax.validation.ConstraintViolationException;
@@ -52,6 +53,14 @@ public class CommonAdvice {
         ConstraintViolationException e) {
         logger.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(InvalidAuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleEntityValidationException(
+        InvalidAuthenticationException e) {
+        logger.error(e.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
             .body(new ErrorResponse(e.getMessage()));
     }
 }
