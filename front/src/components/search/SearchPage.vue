66ee6148c5ec8baec077557f5078b773a4230055
yejiahn <yeppilisa@naver.com>
2020-08-11 17:08:29 +0900

    feature/72 feat: 검색 페이지에 직접 입력 선택지 추가
     - v-date-picker 사용

--- front/src/components/search/SearchPage.vue
+++ front/src/components/search/SearchPage.vue
@@ -18,15 +18,84 @@
       <v-sheet elevation="2" class="time-choices rounded-lg">
         <v-container fluid>
           <v-radio-group dense v-model="radios" :mandatory="false">
-            <v-radio label="전체" value="radio-1" :color="buttonColor"></v-radio>
-            <v-radio label="24시간 이내" value="radio-2" :color="buttonColor"></v-radio>
-            <v-radio label="일주일 이내" value="radio-3" :color="buttonColor"></v-radio>
-            <v-radio label="한 달 이내" value="radio-4" :color="buttonColor"></v-radio>
-            <v-radio label="직접 입력" value="radio-5" :color="buttonColor">
-              <template v-slot:label>
-                <div>직접 입력</div>
-              </template>
-            </v-radio>
+            <v-radio
+              label="전체"
+              value="radio-1"
+              :color="buttonColor"
+            ></v-radio>
+            <v-radio
+              label="한 달 이내"
+              value="radio-2"
+              :color="buttonColor"
+            ></v-radio>
+            <v-radio
+              label="일주일 이내"
+              value="radio-3"
+              :color="buttonColor"
+            ></v-radio>
+            <v-radio
+              label="24시간 이내"
+              value="radio-4"
+              :color="buttonColor"
+            ></v-radio>
+            <v-radio
+              label="직접 입력"
+              value="radio-5"
+              :color="buttonColor"
+            ></v-radio>
+            <template v-if="radios === 'radio-5'">
+              <v-menu
+                v-model="startDateMenu"
+                :close-on-content-click="false"
+                transition="scale-transition"
+                offset-y
+                min-width="290px"
+                max-height="300px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="startDate"
+                    prepend-icon="mdi-calendar"
+                    label="시작 날짜"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="startDate"
+                  no-title
+                  scrollable
+                  @input="startDateMenu = false"
+                ></v-date-picker>
+              </v-menu>
+              <v-menu
+                v-model="endDateMenu"
+                :close-on-content-click="false"
+                transition="scale-transition"
+                offset-y
+                min-width="290px"
+                max-height="330px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="endDate"
+                    prepend-icon="mdi-calendar"
+                    label="종료 날짜"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="endDate"
+                  @input="endDateMenu = false"
+                  no-title
+                  scrollable
+                >
+                </v-date-picker>
+              </v-menu>
+            </template>
           </v-radio-group>
         </v-container>
       </v-sheet>
@@ -36,7 +105,7 @@
       class="my-6 mx-1 rounded-pill"
       :color="buttonColor"
       fixed
-      right="true"
+      right
       bottom
     >
       검색
@@ -53,6 +122,10 @@ export default {
     return {
       radios: "radio-1",
       buttonColor: DORAN_DORAN_COLORS.POINT_COLOR,
+      startDate: new Date().toISOString().substr(0, 10),
+      endDate: new Date().toISOString().substr(0, 10),
+      startDateMenu: false,
+      endDateMenu: false,
     };
   },
   methods: {
@@ -66,19 +139,16 @@ export default {
 <style scoped>
 .input-field-wrapper {
   height: 60px;
-  background-color: #f5f5f5;
 }
 .input-field {
   width: 94%;
   margin: auto;
 }
 .time-filter-wrapper {
-  /*width: 90%;*/
   width: 94%;
   margin: 20px auto;
 }
 .time-choices {
-  /*width: 94%;*/
   margin: 0 auto;
 }
 </style>
