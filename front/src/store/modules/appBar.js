b2fcb81e320fea132c3e7043440cb86437fd53ad
yebink <yebin825@gmail.com>
2020-08-13 16:36:46 +0900

    feature/48 feat: DoranAppBar에 필요한 모든 컴포넌트들을 appBar.js에 추가
     - MapPage에 대한 분기 구현

--- front/src/store/modules/appBar.js
+++ front/src/store/modules/appBar.js
@@ -1,16 +1,48 @@
 export default {
   namespaced: true,
   state: {
+    backButton: false,
+    myPageButton: false,
+
     centerAddress: "",
+
+    timelineButton: false,
+    mapButton: false,
   },
   mutations: {
     CHANGE_ADDRESS(state, newAddress) {
       state.centerAddress = newAddress;
     },
+    MAP_PAGE_DEFAULT_MODE(state) {
+      state.backButton = false;
+      state.myPageButton = true;
+      state.centerAddress = "";
+      state.timelineButton = true;
+      state.mapButton = false;
+    },
+    MAP_PAGE_NON_DEFUALT_MODE(state) {
+      state.backButton = false;
+      state.myPageButton = false;
+      state.centerAddress = "";
+      state.timelineButton = false;
+      state.mapButton = false;
+    },
   },
   getters: {
+    backButton: (state) => {
+      return state.backButton;
+    },
+    myPageButton: (state) => {
+      return state.myPageButton;
+    },
     centerAddress: (state) => {
       return state.centerAddress;
+    },
+    timelineButton: (state) => {
+      return state.timelineButton;
+    },
+    mapButton: (state) => {
+      return state.mapButton;
     },
   },
 };
