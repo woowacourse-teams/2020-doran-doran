54f3aeab6a9bacfd725aed464c948eaff4120264
hwookim <gusdn9754@gmail.com>
2020-08-13 15:18:43 +0900

    feature/72 refactor: DataPickerMenu 컴포넌트 구현
     - SearchPage 내부 불필요한 클래스, 속성 제거, 레이아웃 변경, 컴포넌트 분리

---
+++ front/src/components/search/DataPickerMenu.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-menu
+    :close-on-content-click="false"
+    max-height="290px"
+    offset-y
+    transition="scale-transition"
+    v-model="menu"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <VTextField
+        :label="label"
+        dense
+        prepend-icon="mdi-calendar"
+        readonly
+        v-bind="attrs"
+        v-model="date"
+        v-on="on"
+        class="pr-3"
+      />
+    </template>
+    <VDatePicker @input="selectDate" no-title scrollable v-model="date" />
+  </v-menu>
+</template>
+
+<script>
+export default {
+  name: "DatePickerMenu",
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      menu: false,
+      date: "",
+    };
+  },
+  methods: {
+    selectDate() {
+      this.menu = false;
+      this.$emit("select-date", this.date);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- front/src/components/search/SearchPage.vue
+++ front/src/components/search/SearchPage.vue
@@ -1,81 +1,26 @@
 <template>
-  <div>
-    <div class="input-field-wrapper">
-      <VTextField
-        v-model="keyword"
-        class="input-field"
-        solo
-        clearable
-        loader-height="10px"
-        height="5px"
-        placeholder="검색어를 입력해주세요"
-        prepend-inner-icon="mdi-magnify"
-      />
-    </div>
-    <div class="time-filter-wrapper">
-      <div>시간</div>
-      <v-sheet elevation="2" class="rounded-lg pa-3">
-        <v-radio-group dense v-model="radios" :mandatory="false">
-          <VRadio label="전체" value="date-all" :color="buttonColor" />
-          <VRadio label="24시간 이내" value="date-day" :color="buttonColor" />
-          <VRadio label="일주일 이내" value="date-week" :color="buttonColor" />
-          <VRadio label="한 달 이내" value="date-month" :color="buttonColor" />
-          <VRadio label="직접 입력" value="date-user" :color="buttonColor" />
-          <template v-if="radios === 'date-user'">
-            <v-menu
-              v-model="startDateMenu"
-              :close-on-content-click="false"
-              transition="scale-transition"
-              offset-y
-              min-width="290px"
-              max-height="300px"
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <VTextField
-                  v-model="userStartDate"
-                  prepend-icon="mdi-calendar"
-                  label="시작 날짜"
-                  readonly
-                  v-bind="attrs"
-                  v-on="on"
-                />
-              </template>
-              <VDatePicker
-                v-model="userStartDate"
-                no-title
-                scrollable
-                @input="startDateMenu = false"
-              />
-            </v-menu>
-            <v-menu
-              v-model="endDateMenu"
-              :close-on-content-click="false"
-              transition="scale-transition"
-              offset-y
-              min-width="290px"
-              max-height="330px"
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <VTextField
-                  v-model="userEndDate"
-                  prepend-icon="mdi-calendar"
-                  label="종료 날짜"
-                  readonly
-                  v-bind="attrs"
-                  v-on="on"
-                />
-              </template>
-              <VDatePicker
-                v-model="userEndDate"
-                @input="endDateMenu = false"
-                no-title
-                scrollable
-              />
-            </v-menu>
-          </template>
-        </v-radio-group>
-      </v-sheet>
-    </div>
+  <div class="pa-6">
+    <VTextField
+      v-model="keyword"
+      solo
+      clearable
+      placeholder="검색어를 입력해주세요"
+      prepend-inner-icon="mdi-magnify"
+    />
+    시간
+    <v-sheet elevation="2" class="rounded-lg pa-3">
+      <v-radio-group dense v-model="radios">
+        <VRadio label="전체" value="date-all" :color="buttonColor" />
+        <VRadio label="24시간 이내" value="date-day" :color="buttonColor" />
+        <VRadio label="일주일 이내" value="date-week" :color="buttonColor" />
+        <VRadio label="한 달 이내" value="date-month" :color="buttonColor" />
+        <VRadio label="직접 입력" value="date-user" :color="buttonColor" />
+      </v-radio-group>
+      <div v-if="radios === 'date-user'" class="d-flex flex-row">
+        <DatePickerMenu :label="'시작 날짜'" @select-date="inputStartDate" />
+        <DatePickerMenu :label="'종료 날짜'" @select-date="inputEndDate" />
+      </div>
+    </v-sheet>
     <v-btn
       @click.prevent="search"
       class="my-6 mx-1 rounded-pill"
@@ -92,9 +37,13 @@
 <script>
 import { DORAN_DORAN_COLORS } from "@/utils/constants";
 import { DATE_FILTER_TYPE } from "@/utils/time-filter-type";
+import DatePickerMenu from "@/components/search/DataPickerMenu";
 
 export default {
   name: "SearchPage",
+  components: {
+    DatePickerMenu
+  },
   data() {
     return {
       keyword: "",
@@ -107,6 +56,12 @@ export default {
     };
   },
   methods: {
+    inputStartDate(date) {
+      this.userStartDate = date;
+    },
+    inputEndDate(date) {
+      this.userEndDate = date;
+    },
     async search() {
       const data = {
         keyword: this.keyword,
@@ -128,16 +83,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.input-field-wrapper {
-  height: 60px;
-}
-.input-field {
-  width: 84%;
-  margin: auto;
-}
-.time-filter-wrapper {
-  width: 84%;
-  margin: 20px auto;
-}
-</style>
+<style scoped></style>
