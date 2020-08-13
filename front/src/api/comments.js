06bfb8b6c0c139b9867108136afc36f8d65d6db1
chws <rebeccachoosir@gmail.com>
2020-08-13 18:54:54 +0900

    feature/81 feat: 댓글 좋아요 추가/삭제 api 및 모듈 추가

--- front/src/api/comments.js
+++ front/src/api/comments.js
@@ -5,12 +5,16 @@ const client = axios.create({
   baseURL: API_BASE_URL.EC2 + "/comments",
 });
 
-export const createComment = (newComment) => client.post("", newComment);
-
 const api = (() => {
   const createComment = (newComment) => client.post("", newComment);
+  const createCommentLike = (newCommentLike) =>
+    client.post(`/likes`, newCommentLike);
+  const deleteCommentLike = (commentLikeId) =>
+    client.delete(`/likes/${commentLikeId}`);
   return {
     createComment,
+    createCommentLike,
+    deleteCommentLike,
   };
 })();
 
--- front/src/store/modules/comment.js
+++ front/src/store/modules/comment.js
@@ -6,5 +6,11 @@ export default {
     async createComment(context, newComment) {
       await api.createComment(newComment);
     },
+    async createCommentLike(context, newCommentLike) {
+      await api.createCommentLike(newCommentLike);
+    },
+    async deleteCommentLike(context, commentLikeId) {
+      await api.deleteCommentLike(commentLikeId);
+    },
   },
 };
