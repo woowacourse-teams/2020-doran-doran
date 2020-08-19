ef46512a1e8746840a38e7785b54e57d092d481d
yejiahn <yeppilisa@naver.com>
2020-08-19 18:03:48 +0900

    hotfix/110 fix: PostBoundsRequest DTO에 롬복 setter 추가

--- src/main/java/com/grasshouse/dorandoran/post/service/dto/PostBoundsRequest.java
+++ src/main/java/com/grasshouse/dorandoran/post/service/dto/PostBoundsRequest.java
@@ -3,7 +3,9 @@ package com.grasshouse.dorandoran.post.service.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
