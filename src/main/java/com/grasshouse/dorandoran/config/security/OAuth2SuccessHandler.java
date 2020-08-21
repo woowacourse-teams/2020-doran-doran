3cd75a7cc4f9141e9b37e1ec5d85839952ffa891
juyeongjung <juyeong.dev@gmail.com>
2020-08-21 16:12:34 +0900

    feature/82 refactor: oauth2.success.redirect.url를 properties로 분리

--- src/main/java/com/grasshouse/dorandoran/config/security/OAuth2SuccessHandler.java
+++ src/main/java/com/grasshouse/dorandoran/config/security/OAuth2SuccessHandler.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -16,6 +17,9 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private final JwtTokenProvider provider;
 
+    @Value("${oauth2.success.redirect.url}")
+    private String url;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
         Authentication authentication) throws IOException {
@@ -23,6 +27,6 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         String id = String.valueOf(oauth2User.getAttributes().get("id"));
         String token = provider.createToken(id);
 
-        response.sendRedirect("http://localhost:8081?token=" + token);
+        response.sendRedirect(url + token);
     }
 }
