4c0106e933d0cd1ad210b32d4bc35b664c067898
yejiahn <yeppilisa@naver.com>
2020-08-24 18:25:16 +0900

    feat/128 feat: 현위치 버튼과 새로고침 버튼의 기능 구현 완료

--- front/src/components/map/MapAssistantButtons.vue
+++ front/src/components/map/MapAssistantButtons.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="assistant-buttons">
-    <v-btn small fab @click="setMapToCurrentLocation">
+    <v-btn
+      small
+      fab
+      :retain-focus-on-click="false"
+      @click="setMapToCurrentLocation"
+    >
       <v-icon>mdi-radiobox-marked</v-icon>
     </v-btn>
     <v-btn small fab class="mt-2" @click="refreshPosts">
@@ -13,11 +18,11 @@
 export default {
   name: "MapAssistantButtons",
   methods: {
-    setMapToCurrentLocation() {
-
+    async setMapToCurrentLocation() {
+      await this.$kakaoMap.setCenterByCurrentLocation();
     },
     refreshPosts() {
-
+      this.$store.dispatch("post/loadPosts");
     },
   },
 };
@@ -31,5 +36,8 @@ export default {
   left: 15px;
   bottom: 20px;
   z-index: 9998;
+}
+.v-btn:not(.v-btn--text):not(.v-btn--outlined):hover:before {
+  opacity: 0 !important;
 }
 </style>
