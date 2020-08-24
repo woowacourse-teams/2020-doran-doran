f8a59651d2a94ab704b90e643b9e9b95f30385ae
yejiahn <yeppilisa@naver.com>
2020-08-24 18:08:10 +0900

    feat/128 feat: 현위치 버튼과 새로고침 버튼 들어간 지도 프론트 완성

---
+++ front/src/components/map/MapAssistantButtons.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="assistant-buttons">
+    <v-btn small fab @click="setMapToCurrentLocation">
+      <v-icon>mdi-radiobox-marked</v-icon>
+    </v-btn>
+    <v-btn small fab class="mt-2" @click="refreshPosts">
+      <v-icon>mdi-refresh</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MapAssistantButtons",
+  methods: {
+    setMapToCurrentLocation() {
+
+    },
+    refreshPosts() {
+
+    },
+  },
+};
+</script>
+
+<style scoped>
+.assistant-buttons {
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  left: 15px;
+  bottom: 20px;
+  z-index: 9998;
+}
+</style>
