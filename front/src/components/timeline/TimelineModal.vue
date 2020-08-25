00868533ed4d0acedde15cc7791f821c3e8fd6ba
hwookim <gusdn9754@gmail.com>
2020-08-26 02:38:48 +0900

    feature/122 feat: 타임라인 모달 트랜지션 적용

--- front/src/components/timeline/TimelineModal.vue
+++ front/src/components/timeline/TimelineModal.vue
@@ -1,66 +1,94 @@
 <template>
-  <div class="modal-mask" @click.self="hide">
-    <div class="pa-3 modal-container">
-      <div class="text-center">
-        <span class="ma-auto text-center">타임라인</span>
-        <v-icon class="close-btn" @click="hide">mdi-close</v-icon>
+  <div class="modal-mask" @click.self="slideDown">
+    <transition name="slide-up" @after-leave="close">
+      <div v-if="rendered" class="pa-3 modal-container">
+        <div class="text-center">
+          <span class="ma-auto text-center">타임라인</span>
+          <v-icon class="close-btn" @click="slideDown">mdi-close</v-icon>
+        </div>
+        <PostItem v-for="post in posts" :key="post.id" :post="post" />
       </div>
-      <PostItem v-for="post in posts" :key="post.id" :post="post" />
-    </div>
+    </transition>
   </div>
 </template>
 
 <script>
-import PostItem from "@/components/timeline/PostItem";
+  import PostItem from "@/components/timeline/PostItem";
 
-export default {
-  name: "TimelineModal",
-  components: {
-    PostItem,
-  },
-  computed: {
-    posts() {
-      const kakaoBounds = this.$kakaoMap.getBounds();
-      const bounds = {
-        top: kakaoBounds.ja,
-        bottom: kakaoBounds.ka,
-        left: kakaoBounds.da,
-        right: kakaoBounds.ia,
+  export default {
+    name: "TimelineModal",
+    components: {
+      PostItem,
+    },
+    data() {
+      return {
+        rendered: false,
       };
-      return this.$store.getters["post/postsInBounds"](bounds).reverse();
     },
-  },
-  methods: {
-    hide() {
-      this.$emit("close");
+    computed: {
+      posts() {
+        const kakaoBounds = this.$kakaoMap.getBounds();
+        const bounds = {
+          top: kakaoBounds.ja,
+          bottom: kakaoBounds.ka,
+          left: kakaoBounds.da,
+          right: kakaoBounds.ia,
+        };
+        return this.$store.getters["post/postsInBounds"](bounds).reverse();
+      },
     },
-  },
-};
+    mounted() {
+      this.rendered = true;
+    },
+    methods: {
+      slideDown() {
+        this.rendered = false;
+      },
+      close() {
+        this.$emit("close");
+      },
+    },
+  };
 </script>
 
 <style scoped>
-.modal-mask {
-  position: fixed;
-  z-index: 9999;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-}
+  .modal-mask {
+    position: fixed;
+    z-index: 9999;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
 
-.modal-container {
-  position: absolute;
-  top: 20px;
-  width: 100%;
-  height: calc(100% - 20px);
-  background-color: #fff;
-  border-radius: 15px 15px 0 0;
-}
+  .modal-container {
+    position: absolute;
+    top: 20px;
+    width: 100%;
+    height: calc(100% - 20px);
+    background-color: #fff;
+    border-radius: 15px 15px 0 0;
+  }
 
-.close-btn {
-  position: absolute;
-  top: 10px;
-  right: 10px;
-}
+  .close-btn {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
+
+  .slide-up-enter-active {
+    animation: slide-up 0.5s;
+  }
+  .slide-up-leave-active {
+    animation: slide-up 0.5s reverse;
+  }
+  @keyframes slide-up {
+    0% {
+      transform: translateY(100%);
+    }
+    100% {
+      transform: translateY(0);
+    }
+  }
 </style>
