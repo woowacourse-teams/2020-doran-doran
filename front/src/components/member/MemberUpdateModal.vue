cdcdd729f81cbd60a4d57ad02c0564bb0598fd6b
hwookim <gusdn9754@gmail.com>
2020-08-27 14:45:04 +0900

    hostfix/160 refactor: 불필요한 클래스 삭제

--- front/src/components/member/MemberUpdateModal.vue
+++ front/src/components/member/MemberUpdateModal.vue
@@ -77,17 +77,6 @@ export default {
 </script>
 
 <style scoped>
-.modal-mask {
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 9998;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  text-align: center;
-}
-
 .modal-container {
   width: 90%;
   bottom: 50%;
