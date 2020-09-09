e7f492a87054a0d654efda5bbd50fd89232516af
chws <rebeccachoosir@gmail.com>
2020-09-09 15:31:52 +0900

    feature/188 refactor: 색 없앰

--- front-admin/src/App.vue
+++ front-admin/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <v-app>
-    <v-navigation-drawer class="doran-doran-color" v-model="drawer" app clipped>
+    <v-app-bar app clipped-left height="60px">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>도란도란</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app clipped>
       <v-list>
         <v-list-item
           v-for="navigator in navigators"
@@ -14,12 +20,6 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-
-    <v-app-bar class="doran-doran-color" app clipped-left height="60px">
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>도란도란</v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-app-bar>
   </v-app>
 </template>
 
@@ -33,12 +33,7 @@ export default {
         { to: "/", title: "Home", icon: "mdi-home" },
         { to: "/manage-report", title: "신고 관리", icon: "mdi-bell" },
       ],
-      isLogin: false,
     };
   },
 };
 </script>
-
-<style>
-@import "assets/index.css";
-</style>
