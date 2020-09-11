35a59fc755f31a6ad44f0560227a3668a0d3cdd5
chws <rebeccachoosir@gmail.com>
2020-09-11 16:40:59 +0900

    feature/201 feat: main.js에서 LogRocket init

--- front/src/main.js
+++ front/src/main.js
@@ -5,12 +5,15 @@ import store from "@/store";
 import vuetify from "@/plugins/vuetify";
 import KakaoMap from "@/plugins/kakao-map";
 import VueAnalytics from "vue-analytics";
-import { GOOGLE_ANALYTICS_TRACKING_ID } from "./secure/trackingid";
+import LogRocket from "logrocket";
+import { LOGROCKET_APP_ID, GOOGLE_ANALYTICS_TRACKING_ID } from "./secure/trackingid";
 
 Vue.use(VueAnalytics, {
   id: GOOGLE_ANALYTICS_TRACKING_ID,
   router,
 });
+
+LogRocket.init(LOGROCKET_APP_ID);
 
 Vue.config.productionTip = false;
 Vue.use(KakaoMap);
