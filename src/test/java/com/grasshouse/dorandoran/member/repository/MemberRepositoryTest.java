756597321b8d81481f4ea70e5bb1b012449a949d
yejiahn <yeppilisa@naver.com>
2020-09-20 22:38:43 +0900

    feature/196 refactor: 닉네임 제한사항 관련 Test를 ParameterizedTest로 묶어서 관리

--- src/test/java/com/grasshouse/dorandoran/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/grasshouse/dorandoran/member/repository/MemberRepositoryTest.java
@@ -2,13 +2,13 @@ package com.grasshouse.dorandoran.member.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.grasshouse.dorandoran.member.domain.Member;
 import javax.validation.ConstraintViolationException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -19,30 +19,21 @@ public class MemberRepositoryTest {
     private MemberRepository memberRepository;
 
     @DisplayName("가능한 닉네임들로 Member를 생성한다.")
-    @Test
-    void possibleNicknameTest() {
-        Member member = Member.builder().nickname("일이삼사오육칠팔구십일이삼사오").oAuthId("oauthId").build();
-        Member member2 = Member.builder().nickname("ottffssentottff").oAuthId("oauthId2").build();
-        Member member3 = Member.builder().nickname("123456789012345").oAuthId("oauthId3").build();
-
-        assertAll(
-            () -> assertThat(memberRepository.save(member)).isNotNull(),
-            () -> assertThat(memberRepository.save(member2)).isNotNull(),
-            () -> assertThat(memberRepository.save(member3)).isNotNull()
-        );
+    @ParameterizedTest
+    @ValueSource(strings = {"일이삼사오육칠팔구십일이삼사오", "ottffssentottff", "123456789012345"})
+    void possibleNicknameTest(String nickname) {
+        Member member = Member.builder().nickname(nickname).oAuthId("oauthId").build();
+        assertThat(memberRepository.save(member)).isNotNull();
     }
 
     @DisplayName("[예외] 가능하지 않은 닉네임들로 Member를 생성한다.")
-    @Test
-    void impossibleNicknameTest() {
-        Member member = Member.builder().nickname("일이삼사오육칠팔구십일이삼사오육").build();
-        Member member2 = Member.builder().nickname("ottffssentottffs").build();
-        Member member3 = Member.builder().nickname("1234567890123456").build();
+    @ParameterizedTest
+    @ValueSource(strings = {"일이삼사오육칠팔구십일이삼사오육", "ottffssentottffs", "1234567890123456"})
+    void impossibleNicknameTest(String nickname) {
+        Member member = Member.builder().nickname(nickname).oAuthId("oauthId").build();
         assertThatThrownBy(() -> {
             memberRepository.save(member);
-            memberRepository.save(member2);
-            memberRepository.save(member3);
-        }).isInstanceOf(ConstraintViolationException.class);
+        }).isInstanceOf(ConstraintViolationException.class).hasMessageContaining("닉네임은 15자를");
     }
 
     @AfterEach
