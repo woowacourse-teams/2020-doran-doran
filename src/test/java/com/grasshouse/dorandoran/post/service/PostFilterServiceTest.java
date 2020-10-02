fa3761cc5deadc5a931d7467929c60d27313452c
yebink <yebin825@gmail.com>
2020-10-03 04:41:35 +0900

    feature/239 refactor: 테스트 위치 변경
     - QueryDSL 메소드 테스트를 PostRepositorySupportTest에서 PostFilterServiceTest로 변경

--- src/test/java/com/grasshouse/dorandoran/post/service/PostFilterServiceTest.java
+++ src/test/java/com/grasshouse/dorandoran/post/service/PostFilterServiceTest.java
@@ -1,11 +1,25 @@
 package com.grasshouse.dorandoran.post.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+import com.grasshouse.dorandoran.comment.domain.Comment;
+import com.grasshouse.dorandoran.comment.repository.CommentRepository;
+import com.grasshouse.dorandoran.fixture.AddressFixture;
+import com.grasshouse.dorandoran.fixture.LocationFixture;
+import com.grasshouse.dorandoran.member.domain.Member;
+import com.grasshouse.dorandoran.member.repository.MemberRepository;
+import com.grasshouse.dorandoran.post.domain.Post;
+import com.grasshouse.dorandoran.post.domain.PostLike;
 import com.grasshouse.dorandoran.post.dto.PostFilterRequest;
+import com.grasshouse.dorandoran.post.dto.PostResponse;
+import com.grasshouse.dorandoran.post.repository.PostLikeRepository;
+import com.grasshouse.dorandoran.post.repository.PostRepository;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -18,6 +32,21 @@ import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 @SpringBootTest
 class PostFilterServiceTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private PostLikeRepository postLikeRepository;
+
+    @Autowired
+    private PostFilterService postFilterService;
 
     @Autowired
     private PostFilterService PostFilterService;
@@ -52,7 +81,74 @@ class PostFilterServiceTest {
             .hasSize(searchResult);
     }
 
+    @DisplayName("post.comments, post.likes를 잘 가져오는지 테스트")
+    @Test
+    void findPostWithKeywordAndDate() {
+        Member member1 = SAVE_MEMBER("멤버1", "oAuthId1");
+        Member member2 = SAVE_MEMBER("멤버2", "oAuthId2");
+        Post post = SAVE_POST(member1);
+        SAVE_COMMENT(member1, post, "댓글1");
+        SAVE_COMMENT(member1, post, "댓글2");
+        SAVE_POST_LIKE(member1.getId(), post);
+        SAVE_POST_LIKE(member2.getId(), post);
+
+        PostFilterRequest request = new PostFilterRequest(null, null, null);
+        List<PostResponse> postResponses = postFilterService.showFilteredPosts(request);
+
+        assertThat(postResponses).hasSize(1);
+        assertAll(
+            () -> {
+                assertThat(postResponses.get(0).getComments()).hasSize(2);
+                assertThat(postResponses.get(0).getLikes()).hasSize(2);
+            }
+        );
+    }
+
+    @AfterEach
+    void tearDown() {
+        commentRepository.deleteAll();
+        postRepository.deleteAll();
+        memberRepository.deleteAll();
+    }
+
     private static LocalDateTime toDate(String value) {
         return LocalDateTime.parse(value, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    private Member SAVE_MEMBER(String nickname, String oAuthId) {
+        Member member = Member.builder()
+            .nickname(nickname)
+            .oAuthId(oAuthId)
+            .build();
+        return memberRepository.save(member);
+    }
+
+    private Post SAVE_POST(Member member) {
+        Post post = Post.builder()
+            .content("글")
+            .location(LocationFixture.JAMSIL_STATION)
+            .author(member)
+            .address(AddressFixture.ADDRESS)
+            .authorAddress(AddressFixture.AUTHOR_ADDRESS).build();
+        return postRepository.save(post);
+    }
+
+    private Comment SAVE_COMMENT(Member member, Post post, String content) {
+        Comment comment = Comment.builder()
+            .author(member)
+            .post(post)
+            .content(content)
+            .distance(0.0)
+            .build();
+        post.addComment(comment);
+        return commentRepository.save(comment);
+    }
+
+    private PostLike SAVE_POST_LIKE(Long memberId, Post post) {
+        PostLike postLike = PostLike.builder()
+            .memberId(memberId)
+            .post(post)
+            .build();
+        return postLikeRepository.save(postLike);
     }
 }
