0dd89e4453d64082098b82b5e161d54cf65cdd06
hwookim <gusdn9754@gmail.com>
2020-11-05 20:18:12 +0900

    feature/261 refactor: 실행 환경 별로 key 다르게 찾아가도록 변경

--- script/copy_properties.sh
+++ script/copy_properties.sh
@@ -5,6 +5,6 @@ echo "> copy_properties.sh"
 
 ENVIRONMENT=$(<script/environment.txt)
 
-cp /home/ubuntu/mysecret/dev-key/keystore.p12 ./src/main/resources/keystore.p12
+cp /home/ubuntu/mysecret/$ENVIRONMENT-key/keystore.p12 ./src/main/resources/keystore.p12
 cp /home/ubuntu/mysecret/application-dev.properties ./src/main/resources/application-$ENVIRONMENT.properties
 cp /home/ubuntu/mysecret/application-security.properties ./src/main/resources/application-security.properties
