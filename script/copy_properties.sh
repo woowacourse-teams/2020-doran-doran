d1a59dff38748da21984417119b90dbc23f5243f
hwookim <gusdn9754@gmail.com>
2020-11-10 02:55:31 +0900

    feature/261 fix: trackingid.js 오타 수정

--- script/copy_properties.sh
+++ script/copy_properties.sh
@@ -10,7 +10,7 @@ ENVIRONMENT=$(<script/environment.txt)
 mkdir -p ./front/src/secure
 
 cp /home/ubuntu/mysecret/appkey.js ./front/src/secure/appkey.js
-cp /home/ubuntu/mysecret/tackingid.js ./front/src/secure/tackingid.js
+cp /home/ubuntu/mysecret/trackingid.js ./front/src/secure/trackingid.js
 cp /home/ubuntu/mysecret/$ENVIRONMENT-key/keystore.p12 ./src/main/resources/keystore.p12
 cp /home/ubuntu/mysecret/application-$ENVIRONMENT.properties ./src/main/resources/application-$ENVIRONMENT.properties
 cp /home/ubuntu/mysecret/application-security.properties ./src/main/resources/application-security.properties
